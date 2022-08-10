4342709a5c3963d56a5e8307e92733166c509f34
TaliSofiMato <nataliasofiamatos@gmail.com>
2022-08-10 11:05:37 -0400

    some styling to radio components and starting logic for radio selection and clean up on sidebar.js

--- src/Radio.js
+++ src/Radio.js
@@ -4,17 +4,25 @@ import {
     FormLabel,
     
 } from '@mui/material';
-
+import * as React from 'react';
+import {useState} from 'react'
 
 const Radio = ({value, label}) => {
+    const [selectedRadio, setSelectedRadio] = useState();
+
+    const radioHandler =(e) => {
+        setSelectedRadio(e.target.value)
+    }
+
     return (
         <> 
                 <FormControlLabel
+                    className={'radio'}
                     control={<MUIRadio />}
                     label={label}
-                    value={value} /> 
+                    value={value}
+                    onChange={radioHandler} /> 
         </>
-
     )
 }
 export default Radio;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -13,8 +10,6 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-// import Toolbar from '@mui/material/Toolbar';
 import { RadioGroup } from '@mui/material';
 import Radio from './Radio'
 import './Sidebar.css'
@@ -36,11 +31,9 @@ const Sidebar = (props) => {
         name="radio-buttons-group"
       >
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+          {['Exercise', 'Meal prep', 'Francisco Ready', 'Service Dog app'].map((text) => (
             <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <Radio value={text} label={text} />
-              </ListItemButton>
+                <Radio  className='radio' value={text} label={text} />
             </ListItem>
           ))}
         </List>
